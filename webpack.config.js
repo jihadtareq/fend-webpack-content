7b5680acdaca9e00ad04ca770bccedddae1f6079
Jihad tarek <jihadtarek60@gmail.com>
2020-06-04 12:56:18 +0200

    add the entery point to webpack

---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    //it will be our start point where webpack would build a tree for us
+  entry :"./src/client/index.js",
+}
