b67847b65b24e11ad83883844a15331f5364100f
Jihad tarek <jihadtarek60@gmail.com>
2020-06-04 15:27:23 +0200

    add module

--- webpack.config.js
+++ webpack.config.js
@@ -4,4 +4,16 @@ const webpack = require('webpack');
 module.exports = {
     //it will be our start point where webpack would build a tree for us
   entry :"./src/client/index.js",
+  module : {
+      rules : [
+          {
+              //in this case we're looking for anything ends by .js
+            test:'/\.js$/',
+            exclude : /node_module/,
+            loader:"babel-loader"
+
+          }
+      ]
+
+  },
 }
